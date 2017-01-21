dabc6786d06f8eae5fe5dd79b33a1ad378d1015a
timraasveld <ttimitri@gmail.com>
2017-01-21 14:42:20 +0100

    Test image class addition

--- app/markdown_renderers/html_renderer.rb
+++ app/markdown_renderers/html_renderer.rb
@@ -1,5 +1,6 @@
+# Tests done in spec/helpers/application_helper_spec.rb
 class HTMLRenderer < Redcarpet::Render::HTML
   def image(link, title, content)
-    "<img class='img-responsive' src='#{link}' />"
+    "<img class=\"img-responsive\" alt=\"#{content}\" src=\"#{link}\" />"
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,10 +5,19 @@ describe ApplicationHelper do
     end
   end
 
-  describe '#render_markdown converts markdown to html' do
-    let(:markdown) { '# [A big link](example.com)' }
+  describe '#render_markdown' do
     subject { described_class.render_markdown markdown }
 
-    it { is_expected.to eq "<h1><a href=\"example.com\">A big link</a></h1>\n" }
+    describe '#render_markdown converts markdown to html' do
+      let(:markdown) { '# [A big link](example.com)' }
+
+      it { is_expected.to eq "<h1><a href=\"example.com\">A big link</a></h1>\n" }
+    end
+
+    describe '#render_markdown adds img-responsive class to images' do
+      let(:markdown) { '![some alt text](example.com)' }
+
+      it { is_expected.to eq "<p><img class=\"img-responsive\" alt=\"some alt text\" src=\"example.com\" /></p>\n" }
+    end
   end
 end
